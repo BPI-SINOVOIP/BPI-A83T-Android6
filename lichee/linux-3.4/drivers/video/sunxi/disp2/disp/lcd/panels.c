c2129b9b403ed7b10c78f07e979d192ccb81d1c2
Justin <Justin>
2017-12-06 16:10:34 +0800

    Porting BPI-M3 lichee feature

--- lichee/linux-3.4/drivers/video/sunxi/disp2/disp/lcd/panels.c
+++ lichee/linux-3.4/drivers/video/sunxi/disp2/disp/lcd/panels.c
@@ -9,6 +9,7 @@ extern __lcd_panel_t t27p06_panel;
 extern __lcd_panel_t st7789s_panel;
 extern __lcd_panel_t tft720x1280_panel;
 extern __lcd_panel_t ibt_panel;
+extern __lcd_panel_t S070WV20_MIPI_RGB_panel;
 
 __lcd_panel_t* panel_array[] = {
 	&default_panel,
@@ -18,6 +19,7 @@ __lcd_panel_t* panel_array[] = {
 	&st7789s_panel,
 	&tft720x1280_panel,
 	&ibt_panel,
+	&S070WV20_MIPI_RGB_panel,
 	/* add new panel below */
 
 	NULL,
