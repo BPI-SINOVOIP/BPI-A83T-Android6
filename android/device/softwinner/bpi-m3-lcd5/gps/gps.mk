0b2884f8487e518460e7b52661c3623e1d98ec8f
Justin <Justin>
2017-12-22 14:33:38 +0800

    Add BPI-M3 LCD5 customize moduel

---
+++ android/device/softwinner/bpi-m3-lcd5/gps/gps.mk
@@ -0,0 +1,7 @@
+# gps
+PRODUCT_COPY_FILES += \
+    device/softwinner/bpi-m3-lcd5/gps/gpsconfig.xml:system/etc/gps/gpsconfig.xml \
+    device/softwinner/bpi-m3-lcd5/gps/gpslogd:system/bin/gpslogd \
+    device/softwinner/bpi-m3-lcd5/gps/glgps:system/bin/glgps \
+    device/softwinner/bpi-m3-lcd5/gps/gps.conf:system/etc/gps.conf \
+    device/softwinner/bpi-m3-lcd5/gps/gps.exdroid.so:system/lib/hw/gps.exdroid.so
