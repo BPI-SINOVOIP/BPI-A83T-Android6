69f30f437edcdf40c1aa2fde0273acf1fa156201
Justin <Justin>
2017-12-27 10:28:11 +0800

    Add BPI 5' LCD Feature (LHR050H41_MIPI_RGB)

--- lichee/brandy/u-boot-2011.09/drivers/video_sunxi/sunxi_v3/lcd/panels.c
+++ lichee/brandy/u-boot-2011.09/drivers/video_sunxi/sunxi_v3/lcd/panels.c
@@ -6,12 +6,14 @@ extern __lcd_panel_t default_panel;
 extern __lcd_panel_t lt070me05000_panel;
 extern __lcd_panel_t t27p06_panel;
 extern __lcd_panel_t S070WV20_MIPI_RGB_panel;
+extern __lcd_panel_t LHR050H41_MIPI_RGB_panel;
 
 __lcd_panel_t* panel_array[] = {
 	&default_panel,
 	&lt070me05000_panel,
 	&t27p06_panel,
 	&S070WV20_MIPI_RGB_panel,
+    &LHR050H41_MIPI_RGB_panel,
 	/* add new panel below */
 
 	NULL,
--- lichee/linux-3.4/drivers/video/sunxi/disp2/disp/lcd/panels.c
+++ lichee/linux-3.4/drivers/video/sunxi/disp2/disp/lcd/panels.c
@@ -10,6 +10,7 @@ extern __lcd_panel_t st7789s_panel;
 extern __lcd_panel_t tft720x1280_panel;
 extern __lcd_panel_t ibt_panel;
 extern __lcd_panel_t S070WV20_MIPI_RGB_panel;
+extern __lcd_panel_t LHR050H41_MIPI_RGB_panel;
 
 __lcd_panel_t* panel_array[] = {
 	&default_panel,
@@ -20,6 +21,7 @@ __lcd_panel_t* panel_array[] = {
 	&tft720x1280_panel,
 	&ibt_panel,
 	&S070WV20_MIPI_RGB_panel,
+    &LHR050H41_MIPI_RGB_panel,
 	/* add new panel below */
 
 	NULL,
